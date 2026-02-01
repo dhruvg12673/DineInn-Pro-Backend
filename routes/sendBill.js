12df8376320a60399707aef85b7488479ddb66b6
dhruvg12673 <dhruvgothi9324@gmail.com>
2026-02-01 15:19:58 +0530

    Update sendBill.js

--- routes/sendBill.js
+++ routes/sendBill.js
@@ -12,7 +12,7 @@ router.post('/api/send-bill', async (req, res) => {
 
   const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
